58b0273e62d02ce0a2dbc03ccd55d7a2e49cf637
eason <yul196@pitt.edu>
2018-11-20 17:04:01 -0500

    initial commit

---
+++ routes/products.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var router = express.Router();
+
+var Product = require("../models/Product.js");
+
+/* GET /products listing. */
+router.get("/", function(req, res, next) {
+  Product.find(function(err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+/* POST /products */
+router.post("/", function(req, res, next) {
+  Product.create(req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* GET /products/id */
+router.get("/:id", function(req, res, next) {
+  Product.findById(req.params.id, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* PUT /products/:id */
+router.put("/:id", function(req, res, next) {
+  Product.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE /products/:id */
+router.delete("/:id", function(req, res, next) {
+  Product.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
